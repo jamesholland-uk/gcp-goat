be0c828d1b6d2ed963adf39c525bbe00fca735ba
James Holland <jamoiholland@gmail.com>
2021-08-05 16:38:13 +0100

    reset

--- big_data.tf
+++ big_data.tf
@@ -24,43 +24,4 @@ resource google_bigquery_dataset "dataset" {
     special_group = "allAuthenticatedUsers"
     role          = "READER"
   }
-  labels = {
-    demo-owner           = "james"
-    git_commit           = "1b1dc85dfc698e131bbee6dc5f90c89e453e54e6"
-    git_file             = "big_data_tf"
-    git_last_modified_at = "2021-08-03-20-30-26"
-    git_last_modified_by = "jamoiholland"
-    git_modifiers        = "jamoiholland"
-    git_org              = "jamesholland-uk"
-    git_repo             = "gcp-goat"
-    level                = "production"
-    pci-dss              = "true"
-    team                 = "engineering"
-    yor_trace            = "b64aa118-8f34-4bea-9423-feb704c1932e"
-  }
-}
-
-resource google_sql_database_instance "master_instance1" {
-  name             = "terragoat-${var.environment}-master1"
-  database_version = "POSTGRES_11"
-  region           = var.region
-
-  settings {
-    tier = "db-f1-micro"
-    ip_configuration {
-      ipv4_enabled = true
-      authorized_networks {
-        name  = "WWW"
-        value = "0.0.0.0/0"
-      }
-    }
-    backup_configuration {
-      enabled = false
-    }
-  }
-  settings {
-    ip_configuration {
-      require_ssl = true
-    }
-  }
 }
--- gcs.tf
+++ gcs.tf
@@ -1,14 +1,7 @@
 resource "google_storage_bucket" "terragoat_website" {
-  name                        = "terragot-${var.environment}"
-  force_destroy               = true
+  name          = "terragot-${var.environment}"
+  force_destroy = true
   uniform_bucket_level_access = true
-  labels = {
-    demo-owner = "james"
-    level      = "development"
-    pci-dss    = "true"
-    team       = "engineering"
-    yor_trace  = "8bf62c34-871d-423a-8554-8c75fe03cdbb"
-  }
 }
 
 
